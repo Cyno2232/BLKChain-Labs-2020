e2b75887f7a6fa0174c2ecac274febe62cdbf186
yaoplusplus <45706892+yaoplusplus@users.noreply.github.com>
2020-12-14 20:57:52 +0800

    over

--- ex4/swap_scripts.py
+++ ex4/swap_scripts.py
@@ -18,18 +18,30 @@ from bitcoin.core.script import *
 def coinExchangeScript(public_key_sender, public_key_recipient, hash_of_secret):
     return [
         # fill this in!
+        OP_DEPTH, 2, OP_EQUAL, 
+        OP_IF, 
+        OP_HASH160, hash_of_secret, OP_EQUALVERIFY, public_key_recipient, OP_CHECKSIG, OP_ELSE,
+        2, public_key_sender, public_key_recipient, 2, OP_CHECKMULTISIG,
+        OP_ENDIF
+        
     ]
 
 # This is the ScriptSig that the receiver will use to redeem coins
 def coinExchangeScriptSig1(sig_recipient, secret):
     return [
         # fill this in!
+        sig_recipient,
+        secret
     ]
 
 # This is the ScriptSig for sending coins back to the sender if unredeemed
 def coinExchangeScriptSig2(sig_sender, sig_recipient):
     return [
         # fill this in!
+        OP_0, 
+        sig_sender ,
+        sig_recipient
+        
     ]
 
 #
